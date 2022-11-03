285d61b592baaf5497bfd9403c73913ce9bf0f41
54dbd <845559360@qq.com>
2022-11-03 10:45:17 +0800

    fix:disable constant mouse tracking add: coin.cpp

---
+++ Graphics_Game/Coin.h
@@ -0,0 +1,15 @@
+//
+// Created by Administrator on 3/11/2022.
+//
+
+#ifndef GRAPHICSGAME_COIN_H
+#define GRAPHICSGAME_COIN_H
+
+#include "Brush.h"
+
+class Coin: public Brush {
+
+};
+
+
+#endif //GRAPHICSGAME_COIN_H
