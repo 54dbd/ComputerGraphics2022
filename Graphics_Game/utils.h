a423b1d9012d33f27179391c233c00a907d08f8e
Bastian-crux <std1006966813@163.com>
2022-11-07 22:13:03 +0800

    feat: head collision

--- Graphics_Game/utils.h
+++ Graphics_Game/utils.h
@@ -325,18 +325,27 @@ void initMAP(){
         }
     }
 }
+int jumpHeight(int playerX, int playerY){
+    for (int i = 0; i < 60; ++i) {
+        if (MAP[playerX][playerY-i].getColor()==Qt::black || MAP[playerX][playerY-i].getColor()==Qt::black
+        || MAP[playerX + 15][playerY-i].getColor()==Qt::black || MAP[playerX + 15][playerY-i].getColor()==Qt::black)
+            return i + 3;
+    }
+    return 60;
+}
+
 bool canJump(int playerX, int playerY){
     if(MAP[playerX+8][playerY+32].getColor()==Qt::black
        ||MAP[playerX+8][playerY+33].getColor()==Qt::black
        ||MAP[playerX+9][playerY+32].getColor()==Qt::black
        ||MAP[playerX+9][playerY+33].getColor()==Qt::black
        ||MAP[playerX+10][playerY+32].getColor()==Qt::black
-       ||MAP[playerX+10][playerY+33].getColor()==Qt::black
-            ){
+       ||MAP[playerX+10][playerY+33].getColor()==Qt::black){
         return true;
     }
     return false;
 }
+
 
 bool canDrop(int playerX, int playerY){
     //x,y从人物左上角开始算,人物高32（16*2）。脚底为x+9，y+32
